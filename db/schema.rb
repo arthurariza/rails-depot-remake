dcef40e5f30184d280202ccb215604b2ef92adc1
arthurariza <tucaariza@gmail.com>
2022-10-25 19:54:17 -0300

    Task A: Product Scaffold With Tailwind

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_220914) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_224919) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
